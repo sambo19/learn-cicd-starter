72aa5f66c2dabfbc16f97c13c400ce9b4d34047d
sambo19 <sam.robillard1@gmail.com>
2025-08-11 18:50:11 -0700

    Add dummy UT

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,7 +8,7 @@ import (
 func TestGetAPIKeyException(t *testing.T) {
 	testHeader := http.Header{}
 	apiKey, _ := GetAPIKey(testHeader)
-	if apiKey -= "" {
+	if apiKey != "" {
 		t.Error("Expected this to fail")
 	}
 }
